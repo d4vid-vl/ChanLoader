b19c8753cef649982637a5b816edfb3f43e6c608
David <157435386+d4vid-vl@users.noreply.github.com>
2024-09-13 18:57:24 -0300

    TODO: Add other formats, only ID is working

---
+++ cmd/custom/name.go
@@ -0,0 +1,41 @@
+package custom
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/imroc/req"
+)
+
+func NameFiles(path string, url string, nameformat string, id string) string {
+	if url != "" {
+		progress := func(current, total int64) {
+			fmt.Println(float32(current)/float32(total)*100, "%")
+		}
+		name := "/"
+		url_split := strings.SplitAfter(url, ".")
+		file_split := strings.Split(url_split[len(url_split)-2], "/")
+
+		if nameformat == "numeric" {
+			name += url_split[len(url_split)-2]
+		} else if nameformat == "alphabetic" {
+			name += url_split[len(url_split)-2]
+		} else if nameformat == "id" {
+			name += id + "."
+		} else if nameformat == "random" {
+			name += url_split[len(url_split)-2]
+		} else if nameformat == "original" {
+			name += file_split[len(file_split)-1]
+		}
+
+		new_path := path + name + url_split[len(url_split)-1]
+		working_url := "https:" + url
+
+		r, _ := req.Get(working_url, req.DownloadProgress(progress))
+		r.ToFile(new_path)
+		fmt.Println("File saved in: " + new_path)
+		return new_path
+	} else {
+		return "No media found in post: " + id
+	}
+}
