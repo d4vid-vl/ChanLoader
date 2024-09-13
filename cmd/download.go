c8770c934ebac10b8182351a1256491d8de45649
David <157435386+d4vid-vl@users.noreply.github.com>
2024-09-13 00:21:12 -0300

    TODO: Finish convertion and downloader
    Plus, verify if image converter works, add download desc, finish
    scraping status

--- cmd/download.go
+++ cmd/download.go
@@ -1,13 +1,14 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
-
-	"log"
 
 	"ChanLoader/cmd/ui/textinput"
 )
@@ -19,20 +20,18 @@ var downloadCmd = &cobra.Command{
 	Run:   download,
 }
 
+// ? Structs to save configurations of the program
 type Save struct {
-	Url        *textinput.Output
-	Path       string
-	ThreadName string
-	ThreadID   string
-	Board      string
+	Url *textinput.Output
+}
+type cfg struct {
+	Url, Path, ThreadName, ThreadID, Board string
 }
 
-type cfg struct {
-	Url        string
-	Path       string
-	ThreadName string
-	ThreadID   string
-	Board      string
+// ? Struct to save each individual post in the thread
+type Post struct {
+	OP                                          bool
+	Subject, Image, Name, Date, PostID, Message string
 }
 
 func init() {
@@ -43,7 +42,9 @@ func init() {
 
 func download(cmd *cobra.Command, args []string) {
 
+	// * Init
 	flagURL := cmd.Flag("url").Value.String()
+	config_path := "config/ConfigData.json"
 
 	save := Save{
 		Url: &textinput.Output{},
@@ -53,6 +54,7 @@ func download(cmd *cobra.Command, args []string) {
 		Url: flagURL,
 	}
 
+	// ! Save URL in local variable
 	tprogram_url := tea.NewProgram(textinput.InitialTextInputModel(save.Url, "Send the URL of the thread you want to save"))
 	if _, err := tprogram_url.Run(); err != nil {
 		cobra.CheckErr(err)
@@ -64,19 +66,50 @@ func download(cmd *cobra.Command, args []string) {
 		log.Fatal("failed to set the url flag value \n", err)
 	}
 
+	// * Check if it is a valid 4Chan thread link
+	if !strings.Contains(cfg.Url, "boards.4chan.org") {
+		log.Fatal("Url given is not a valid 4chan thread. (Invalid link)")
+	} else if !strings.Contains(cfg.Url, "thread") {
+		log.Fatal("Url given is not a valid 4chan thread. (Not a thread)")
+	}
+
 	split_url := strings.Split(cfg.Url, "/")
-	fmt.Println(split_url)
 
 	// ! Save Board and Thread ID info
 	for i := 0; i < len(split_url); i++ {
 		check := split_url[i]
-		if check == "board.4chan.org" {
-			save.Board = split_url[i+1]
+		if check == "boards.4chan.org" {
+			cfg.Board = split_url[i+1]
 		} else if check == "thread" {
-			save.ThreadID = split_url[i+1]
+			cfg.ThreadID = split_url[i+1]
 		} else {
 			continue
 		}
 	}
 
+	// ! Read and write config path in local variable
+	content, err := os.ReadFile(config_path)
+	if err != nil {
+		log.Fatal("Couldn't get file path correctly \n", err)
+	}
+	var config Config
+	if err := json.Unmarshal(content, &config); err != nil {
+		fmt.Println("Couldn't read json file", err)
+	}
+	cfg.Path = config.Path
+	fmt.Println(cfg)
+
+	// ! Create board and thread folder
+	thread_path := cfg.Path + "/" + cfg.Board + "/" + cfg.ThreadID
+	err_thread_path := os.MkdirAll(thread_path, os.ModePerm)
+	if err_thread_path != nil {
+		log.Fatal("Error creating thread folder \n", err_thread_path)
+	}
+
+	// * Scraping state
+	test := scrapeurl(cfg.Url, cfg.ThreadID)
+	for i := 0; i < len(test); i++ {
+		// TODO: Name -> Image -> Video
+		// TODO: Create name downloader and video converter
+	}
 }
---
+++ custom/image/image.go
@@ -0,0 +1,44 @@
+package image
+
+import (
+	"io"
+	"log"
+	"strings"
+
+	"github.com/sunshineplan/imgconv"
+)
+
+func convertImage(path string, format string) string {
+	src, err := imgconv.Open(path)
+	if err != nil {
+		log.Fatalf("failed to open image: %v", err)
+	}
+
+	path_format := strings.SplitAfter(path, ".")
+
+	// * Check if the file format is the same as the converted one
+	if path_format[len(path_format)] != format {
+		// * Convertion
+		if format == "png" {
+			if err := imgconv.Write(io.Discard, src, &imgconv.FormatOption{Format: imgconv.PNG}); err != nil {
+				log.Fatalf("failed to write image: %v", err)
+			}
+		} else if format == "jpeg" {
+			if err := imgconv.Write(io.Discard, src, &imgconv.FormatOption{Format: imgconv.JPEG}); err != nil {
+				log.Fatalf("failed to write image: %v", err)
+			}
+		} else if format == "webp" {
+			if err := imgconv.Write(io.Discard, src, &imgconv.FormatOption{Format: imgconv.PNG}); err != nil {
+				log.Fatalf("failed to write image: %v", err)
+			}
+		}
+	}
+	var new_path string
+	for i := 0; i < len(path_format)-1; i++ {
+		new_path += path_format[i]
+	}
+	new_path += format
+	return new_path
+
+	// TODO: Verify if is working
+}
