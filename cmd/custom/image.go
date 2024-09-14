968ce0b1da299132d1fe6e5c7837ba4116b69fa9
David <157435386+d4vid-vl@users.noreply.github.com>
2024-09-14 18:11:03 -0300

    Changed libraries
    Now image and video convertions require ffmpeg to be installed

--- cmd/custom/image.go
+++ cmd/custom/image.go
@@ -1,44 +1,69 @@
 package custom
 
 import (
-	"io"
 	"log"
+	"os"
 	"strings"
 
-	"github.com/sunshineplan/imgconv"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func ConvertImage(path string, format string) string {
-	src, err := imgconv.Open(path)
-	if err != nil {
-		log.Fatalf("failed to open image: %v", err)
-	}
 
-	path_format := strings.SplitAfter(path, ".")
-
-	// * Check if the file format is the same as the converted one
-	if path_format[len(path_format)] != format {
-		// * Convertion
-		if format == "png" {
-			if err := imgconv.Write(io.Discard, src, &imgconv.FormatOption{Format: imgconv.PNG}); err != nil {
-				log.Fatalf("failed to write image: %v", err)
-			}
-		} else if format == "jpeg" {
-			if err := imgconv.Write(io.Discard, src, &imgconv.FormatOption{Format: imgconv.JPEG}); err != nil {
-				log.Fatalf("failed to write image: %v", err)
-			}
-		} else if format == "webp" {
-			if err := imgconv.Write(io.Discard, src, &imgconv.FormatOption{Format: imgconv.PNG}); err != nil {
-				log.Fatalf("failed to write image: %v", err)
-			}
-		}
+	// * Create the new path
+	file_path := strings.SplitAfter(path, "/")
+	var convert_path string
+	for i := 0; i < len(file_path)-1; i++ {
+		convert_path += file_path[i]
 	}
+	convert_path += "Images/"
+	err_convert_path := os.MkdirAll(convert_path, os.ModePerm)
+	if err_convert_path != nil {
+		log.Fatal("Error creating converting images folder \n", err_convert_path)
+	}
+	convert_path += file_path[len(file_path)-1] // New path with old file
+
+	path_format := strings.SplitAfter(convert_path, ".")
+
 	var new_path string
 	for i := 0; i < len(path_format)-1; i++ {
 		new_path += path_format[i]
 	}
-	new_path += format
-	return new_path
+	new_path += format // New path with new file
 
-	// TODO: Verify if is working
+	// * Check if the format is an image format
+	if path_format[len(path_format)-1] == "png" || path_format[len(path_format)-1] == "jpg" || path_format[len(path_format)-1] == "webp" || path_format[len(path_format)-1] == "jpeg" {
+		// * Check if the file format is the same as the converted one
+		if path_format[len(path_format)-1] != format {
+
+			// * Convertion
+			if format == "png" {
+				err := ffmpeg.Input(path).Output(new_path, ffmpeg.KwArgs{"loglevel": "panic"}).OverWriteOutput().ErrorToStdOut().Run()
+				if err != nil {
+				}
+			} else if format == "jpeg" {
+				err := ffmpeg.Input(path).Output(new_path, ffmpeg.KwArgs{"loglevel": "panic"}).OverWriteOutput().ErrorToStdOut().Run()
+				if err != nil {
+				}
+			} else if format == "webp" {
+				err := ffmpeg.Input(path).Output(new_path, ffmpeg.KwArgs{"loglevel": "panic"}).OverWriteOutput().ErrorToStdOut().Run()
+				if err != nil {
+				}
+			}
+
+			// * Remove the original file
+			if err := os.Remove(path); err != nil {
+			}
+			// TODO: Remove the println
+			return new_path
+
+		} else {
+			// * Move original file to images
+			if err := os.Rename(path, convert_path); err != nil {
+			}
+			return convert_path
+		}
+	} else {
+		return ""
+	}
 }
